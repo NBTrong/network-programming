9ea6cfaff92953f776d6da433f009b04d379f677
NBTrong <trong.nb121001@gmail.com>
2023-11-25 14:09:07 +0700

    Init lab7

---
+++ lab7/TCP_Server/src/feature/Article/article.c
@@ -0,0 +1,26 @@
+#include "article.h"
+
+// -------------------------------------- Post function ------------------------------------------------
+
+void postArticle(int client_socket, char *article)
+{
+    char buffer[STRING_LENGTH];
+
+    // Check login status
+    if (checkLoginStatus(client_socket) == NOT_LOGGED_IN)
+    {
+        send_with_error_handling(
+            client_socket,
+            buffer,
+            int_to_string(NOT_HAVE_ACCESS),
+            "Send message login status error");
+        return;
+    }
+    send_with_error_handling(
+        client_socket,
+        buffer,
+        int_to_string(POST_SUCCESSFULLY),
+        "Send message login status error");
+
+    printf("Client %d post article: %s\n", client_socket, article);
+}
