3f6a617ababa9cf9c7baf530991d529e5587554e
NBTrong <trong.nb121001@gmail.com>
2023-12-21 14:30:00 +0700

    feature: Implement Authentication function final project

--- final-project/TCP_Client/src/feature/Article/article.c
+++ final-project/TCP_Client/src/feature/Article/article.c
@@ -1,5 +1,49 @@
 #include "article.h"
 
+void processUserList(const char *userList)
+{
+  // Create a copy of the input string that can be modified
+  char userListCopy[STRING_LENGTH];
+  strncpy(userListCopy, userList, sizeof(userListCopy));
+
+  char *token = strtok(userListCopy, " "); // Split the first username
+  int userCount = 0;
+
+  while (token != NULL)
+  {
+    printf("%s online\n", token);
+    token = strtok(NULL, " ");
+    userCount++;
+  }
+
+  // Check if no users are online and replace with a space if needed
+  if (userCount == 0)
+  {
+    printf("No users online\n");
+  }
+}
+
+void getListUserOnline(int socket)
+{
+  char buffer[STRING_LENGTH];
+  char message[STRING_LENGTH + 5];
+
+  sprintf(message, "ONLINE 1");
+
+  send_with_error_handling(
+      socket,
+      buffer,
+      message,
+      "Send message login status error");
+
+  recv_with_error_handling(
+      socket,
+      buffer,
+      sizeof(buffer),
+      "Error receiving data from the client");
+  processUserList(buffer);
+}
+
 void postArticle(int socket)
 {
   char article[STRING_LENGTH];
